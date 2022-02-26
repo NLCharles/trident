cef619d1c3612bc13da21654c0021ca182e0c8ff
cwman <qzh19950601@gmail.com>
2022-02-26 21:48:00 +0800

    add numpy example

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ if __name__ == '__main__':
                 module='trident.example',
                 sources=[
                     'core/example_torch.cpp',
+                    'core/example_numpy.cpp',
                 ]
             ),
         ],
--- trident/example/core/example_torch.cpp
+++ trident/example/core/example_torch.cpp
@@ -4,16 +4,21 @@
 #include <vector>
 #include <cuda.h>
 #include <cuda_runtime_api.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
+namespace py = pybind11;
 namespace Trident{
     void HelloTrident(){
         std::cout<<"Hello Trident!\nTrident is a framework for fast GPU operation development, test and deploy!\n";
         return;
     }
+    py::array_t<double> add_arrays(py::array_t<double> input1, py::array_t<double> input2);
 }
 
 using namespace Trident;
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
 	m.def("hello", &HelloTrident, "trident.hello : \n A helloworld example of trident.");
+    m.def("add_arrays", &add_arrays, "Add two NumPy arrays");
 }
